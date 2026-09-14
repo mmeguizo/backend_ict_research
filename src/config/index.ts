99e32cc40e10a81a5b321601132710ad3dcff861
Mark Oliver Meguizo <mark.meguizo@gmail.com>
2026-09-14 16:02:18 +0800

    iddate

--- src/config/index.ts
+++ src/config/index.ts
@@ -19,6 +19,8 @@ export const config = {
       "http://10.100.168.9:4201",
       "https://10.100.168.9:55001",
       "https://frontend-ict-research-7k53ed322-mmeguizos-projects.vercel.app",
+      "https://frontend-ict-research-mbcdxjd4c-mmeguizos-projects.vercel.app",
+      "https://frontend-ict-research.vercel.app/login",
     ],
     credentials: true,
   },
